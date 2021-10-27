c69688971dbbc6e433ccd92ebf1d878055620734
Scott Lobdell <slobdell@google.com>
2021-10-27 23:12:58 +0000

    Temporarily disable conditional for tradefederation/core
    tradefederation/core cannot be added into the source tree with this
    conditional in place. The project must first be added so that its
    contents can subsequently be synced
    
    Change-Id: I2f46d431d290a9d38d3bedd488c653ac95da1951

--- test_harness/Android.mk
+++ test_harness/Android.mk
@@ -15,7 +15,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-ifeq (,$(wildcard $(LOCAL_PATH)/../../core))
+# ifeq (,$(wildcard $(LOCAL_PATH)/../../core))
+# TODO(b/204371734) temporarily disable this conditional until tradefederation/core is bootstrapped into the tree
+ifeq (,$(wildcard $(LOCAL_PATH)/../../doesnotexist))
 .PHONY: tradefed-core
 tradefed-core: tradefed atest_tradefed.sh
 .PHONY: tradefed-all
